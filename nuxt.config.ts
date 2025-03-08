46ba5c8719afcc661ab684b2846f164fe5676dc5
Jack Christian <ccontact@jackchristian.uk>
2025-03-08 15:13:12 +1100

    meta and htmlk content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,18 @@ export default defineNuxtConfig({
 
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
 
+  app: {
+    head: {
+      title: 'Accessible Web Media',
+      meta: [
+        { key: 'description', content: 'Your all-in-one web media accessibility platform. Describe images, caption videos and transcribe audio here.' }
+      ],
+      htmlAttrs: {
+        lang: 'en'
+      }
+    }
+  },
+
   runtimeConfig: {
     replicateToken: '',
     deepgramKey: '',
